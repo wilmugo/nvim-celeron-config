adf775908f28d76ea4a9cd4c9428c1743adca39c
Wilfredo Murillo <wilmugo@gmail.com>
2024-01-07 01:43:29 -0400

    modificadas las opciones en las keys del plugin fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,7 +2,7 @@ return {
   "tpope/vim-fugitive",
   cmd = { "G", "Git" },
   keys = {
-    { "<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch" },
-    { "<leader>gl", ":Git pull<cr>", desc = "Git pull" },
+    { "<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch", { noremap = true, silent = true } },
+    { "<leader>gl", ":Git pull<cr>", desc = "Git pull", { noremap = true, silent = true } },
   },
 }
